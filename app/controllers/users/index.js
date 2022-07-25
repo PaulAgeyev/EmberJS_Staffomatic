ade30eda6c72c8a0d54e19fbdb0a08b671a1ddd7
Pavlo Ahieiev (pahieiev) <pahieiev@cisco.com>
2022-07-25 14:44:19 +0300

    Resolve prettier issue

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -8,8 +8,8 @@ export default class UsersController extends Controller {
 
   @action
   toggleUsers() {
-    const transitionTo
-      = (queryParams) => this.target.transitionTo('users', queryParams);
+    const transitionTo = (queryParams) =>
+      this.target.transitionTo('users', queryParams);
 
     if (this.archived) {
       transitionTo({ queryParams: { archived: undefined } });
